e7fc61c5d2fe0a8f1a5ff4fee459cbff806cc3f2
= <=>
2024-04-23 13:00:02 +0530

    A Back =>  Button has  been added sucessfully in footyerinfo for navigate from footerInfo page to Home page

--- Ott-Platform/fontend/src/Components/Footer/FooterInfo.jsx
+++ Ott-Platform/fontend/src/Components/Footer/FooterInfo.jsx
@@ -1,20 +1,38 @@
-import { useContext } from "react"
-import FooterContext from "../../Context/FooterContext"
+import { useContext } from "react";
+import FooterContext from "../../Context/FooterContext";
+import { FaHome } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+
+
+
+
 
 function FooterInfo() {
-    const { footerContextDetails } = useContext(FooterContext)
-    let heading = footerContextDetails.heading
-    let title = footerContextDetails.title
+    const { footerContextDetails } = useContext(FooterContext);
+    let heading = footerContextDetails.heading;
+    let title = footerContextDetails.title;
+    const navigate = useNavigate()
 
-    return (
-        <div className="bg-gray-900 w-full min-h-[100vh] text-white px-10 py-10">
-            
-            <h1 className="text-3xl hover:border-b-2 inline transition-all duration-150 cursor-pointer">{heading}</h1>
-            
-            <p className="mt-5 ml-8">{title}</p>
-            
-        </div>
-    ) 
+    const navigateToHomePage = () => {
+        navigate('/')
+    }
+
+  return (
+    <div className="bg-gray-900 w-full min-h-[100vh] text-white px-10 py-10">
+      <div
+        onClick={navigateToHomePage}
+        className="text-3xl absolute right-10 top-2 hover:bg-green-600 px-3 py-2 rounded-md"
+      >
+        <FaHome />
+      </div>
+
+      <h1 className="text-3xl hover:border-b-2 inline transition-all duration-150 cursor-pointer">
+        {heading}
+      </h1>
+
+      <p className="mt-5 ml-8">{title}</p>
+    </div>
+  );
 }
 
-export {FooterInfo}
+export { FooterInfo };
--- Ott-Platform/fontend/src/Components/Screen4/FullPage.Screen4.jsx
+++ Ott-Platform/fontend/src/Components/Screen4/FullPage.Screen4.jsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 import movieBannerObj from "./ImagesContainer.Screen4.js";
 import { FaPlay } from "react-icons/fa";
 
-function Cart({ imageUrl  }) {
+function Cart({ imageUrl  , customStyle}) {
   
   const [isPlayBtn, setIsPlayBtn] = useState(false);
   return (
     <div
       onMouseEnter={() => setIsPlayBtn(true)}
       onMouseLeave={() => setIsPlayBtn(false)}
-      className={`w-full lg:w-[15%] relative rounded-md h-[250px] border-[0.5px] border-gray-800  transition duration-300 ease-in-out transform hover:scale-[1.2] shadow-2xl hover:shadow-lg hidden lg:block`}
+      className={`w-full lg:w-[15%] relative rounded-md h-[250px] border-[0.5px] border-gray-800  transition duration-300 ease-in-out transform hover:scale-[1.05] lg:hover:scale-[1.2] shadow-2xl hover:shadow-lg ${customStyle}`}
     >
       <img src={imageUrl} alt="" className="w-full h-full object-cover" />
 
@@ -38,7 +38,7 @@ export default function FullPage_Screen4() {
     image12 ,
     image13 ,
     image14 ,
-    image15
+    image15 ,
   } = movieBannerObj;
 
   return (
@@ -46,21 +46,21 @@ export default function FullPage_Screen4() {
       <h2 className="text-2xl text-white font-bold pl-10">Recommended Movies</h2>
     
     <div className="bg-black px-4 lg:p-10 pt-20  flex flex-col lg:flex-row flex-wrap justify-between gap-x-10 gap-y-8">
-      <Cart imageUrl={image1} />
-      <Cart imageUrl={image2} />
-      <Cart imageUrl={image3} />
-      <Cart imageUrl={image4} />
-      <Cart imageUrl={image5} />
-      <Cart imageUrl={image6} />
-      <Cart imageUrl={image7} customStyle="hidden lg:visible"/>
-      <Cart imageUrl={image8} customStyle="hidden lg:visible" />
-      <Cart imageUrl={image9}  customStyle="hidden lg:visible"/>
-      <Cart imageUrl={image10}  customStyle="hidden lg:visible"/>
-      <Cart imageUrl={image11}  customStyle="hidden lg:visible"/>
-      <Cart imageUrl={image12}  customStyle="hidden lg:visible"/>
-      <Cart imageUrl={image13}  customStyle="hidden lg:visible"/>
-      <Cart imageUrl={image14}  customStyle="hidden lg:visible"/>
-      <Cart imageUrl={image15}  customStyle="hidden lg:visible"/>
+      <Cart imageUrl={image1} customStyle="hidden lg:block"/>
+      <Cart imageUrl={image2} customStyle="hidden lg:block"/>
+      <Cart imageUrl={image3} customStyle="hidden lg:block"/>
+      <Cart imageUrl={image4} customStyle="hidden lg:block"/>
+      <Cart imageUrl={image5} customStyle=""/>
+      <Cart imageUrl={image6} customStyle="hidden lg:block"/>
+      <Cart imageUrl={image7}   customStyle="hidden lg:block"/>
+      <Cart imageUrl={image8}   customStyle="hidden lg:block" />
+      <Cart imageUrl={image9}   customStyle=""/>
+      <Cart imageUrl={image10}  customStyle=""/>
+      <Cart imageUrl={image11}  customStyle=""/>
+      <Cart imageUrl={image12}  customStyle=""/>
+      <Cart imageUrl={image13}  customStyle=""/>
+      <Cart imageUrl={image14}  customStyle="hidden lg:block"/>
+      <Cart imageUrl={image15}  customStyle="hidden lg:block"/>
     </div>
     </div>
   );
